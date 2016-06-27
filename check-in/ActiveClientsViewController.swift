a0700312e5d0614365baa801cf4c25acc6cceed8
JediMaster <jedimaster@Owners-iMac.local>
2016-06-27 16:17:23 -0600

    6-27@4-17

---
+++ check-in/ActiveClientsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ActiveClientsViewController.swift
+//  check-in
+//
+//  Created by JediMaster on 6/26/16.
+//  Copyright © 2016 JediMaster. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ActiveClientsViewController {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
